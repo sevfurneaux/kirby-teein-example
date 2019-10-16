b817c95024ed67f925997aa506c3d06986bdd2f8
Sev Furneaux <sevfurneaux@gmail.com>
2019-10-16 14:30:45 +0100

    Update default.php

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,12 +1,10 @@
 <?php
 
-use function Teein\Html\Elements\{div, span};
-use function Teein\Html\Attributes\{class_};
+use function Teein\Html\Elements\div;
+use function Teein\Html\Attributes\class_;
 use function Teein\Html\Text\text;
 use function Test\Components\layout;
 use function Test\Components\grid;
-
-// require __DIR__ . "/components/layout.php";
 
 return layout(
     div()(
